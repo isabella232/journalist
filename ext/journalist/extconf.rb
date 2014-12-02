a535ff6508e0fe212c86b7b02bacab83c0efc4e3
Kir Shatrov <shatrov@me.com>
2014-12-02 10:14:38 -0500

    Check ruby version before compilation

--- ext/journalist/extconf.rb
+++ ext/journalist/extconf.rb
@@ -1,5 +1,10 @@
 require "mkmf"
 
+ruby = RUBY_VERSION.split('.').map{ |s| s.to_i }
+if ruby[0] < 2 || (ruby[0] == 2 && ruby[1] < 1)
+  abort "----\nJournalist requires MRI >= 2.1\n----"
+end
+
 yajl_inc, yajl_lib = pkg_config('yajl')
 abort "----\nCannot find YAJL\n----" unless yajl_inc && yajl_lib
 
